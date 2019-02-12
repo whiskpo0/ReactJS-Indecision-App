ab0be007e3c617e755dda8363168a57cb25a5874
whiskpo0 <whiskypo082@gmail.com>
2019-02-12 14:08:28 -0600

    just adding fluff

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,50 @@
 console.log('App.js is running!');
 
 // JSX - JavaScript  XML
+// var template = ( 
+//     <div>
+//         <h1>Does this change?</h1>
+//         <p>This is some </p>
+//         <ol>
+//             <li>Item one </li>
+//             <li>Item two </li>
+//         </ol>
+//     </div>
+// ); 
+
+var user = {
+    name: 'Adam Mathisom',
+    age: 36,
+    location: 'Minnesota'
+};
+
+var userName = 'Mike';
+var userAge = 27;
+var userLocation = 'New York';
+
 var template = React.createElement(
-  'p',
-  null,
-  'Does this change?'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location,
+        ' '
+    )
 );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 console.log('App.js is running!'); 
 
 // JSX - JavaScript  XML
-var template = <p>Does this change?</p>; 
+// var template = ( 
+//     <div>
+//         <h1>Does this change?</h1>
+//         <p>This is some </p>
+//         <ol>
+//             <li>Item one </li>
+//             <li>Item two </li>
+//         </ol>
+//     </div>
+// ); 
+
+var user = { 
+    name: 'Adam Mathisom', 
+    age : 36, 
+    location: 'Minnesota'
+}; 
+
+var userName = 'Mike';
+var userAge = 27;
+var userLocation = 'New York'; 
+
+var template = ( 
+    <div>
+        <h1>{ user.name }</h1>
+        <p>Age: { user.age }</p>
+        <p>Location: { user.location } </p>
+    </div>
+); 
+
 var appRoot = document.getElementById('app'); 
 
 ReactDOM.render(template, appRoot); 
